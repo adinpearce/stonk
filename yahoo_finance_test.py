5b13df2a384e1c32a2e8761a658764b2520b1123
retr01 <adin2427@gmail.com>
2021-05-25 20:44:20 +0800

    (yahoo_finance)update

--- yahoo_finance_test.py
+++ yahoo_finance_test.py
@@ -16,12 +16,11 @@ def time_counter(a, b):
 
     c_time = b_time - a_time
 
-    diff_time = str(c_time).strip(" days, 0:00:00")
+    diff_time = c_time.days
 
     diff_time = int(diff_time)
 
-    diff_time = diff_time - 8
-    seconds = diff_time * 24 * 60 * 60
+    seconds = diff_time * 24 * 60 * 60 - 28800
 
     return seconds
 
@@ -30,20 +29,29 @@ def retriver(stock_num, period1, period2, state):
     url = "https://query1.finance.yahoo.com/v8/finance/chart/"+str(stock_num)+".TW?period1="+str(
         period1)+"&period2="+str(period2)+"&interval=1d&events=history&=hP2rOschxO0"
 
+    print(url)
+    print("="*50)
+
     response = requests.get(url)
     return_data = json.dumps(response.text)
-    return return_data
+    return response.text
 
 
 def json_extraction(json_data):
-    #data = json.loads(json_data)
-    json_data = dict(json_data)
-    got_data = json_data["low"]
-    return got_data
+    data = json.loads(json_data)
+
+    open = data['chart']['result'][0]['indicators']['quote'][0]['open']
+    close = data['chart']['result'][0]['indicators']['quote'][0]['close']
+    low = data['chart']['result'][0]['indicators']['quote'][0]['low']
+    high = data['chart']['result'][0]['indicators']['quote'][0]['high']
+    volume = data['chart']['result'][0]['indicators']['quote'][0]['volume']
+    return open, close, low, high, volume
 
 
-period1 = time_counter(a, "2021-05-20")
-period2 = time_counter(a, time_combination)
+period1 = time_counter(a, "2021-05-25")
+period2 = time_counter(a, "2021-05-26")
+
+print(period1, period2)
 
 #print(retriver(2330, period1, period2, 0))
 print(json_extraction(retriver(2330, period1, period2, 0)))
